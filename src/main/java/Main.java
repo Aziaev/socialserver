e1e39a2621ed1fe5963b00b31361d31f612d100d
Aziaev <aziaev@gmail.com>
2016-11-13 19:35:42 +0300

    https://github.com/Aziaev/socialserver.git

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+
+/**
+ * Created by Artur.Ziaev on 13.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server(9090);
+
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase();
+    }
+}
